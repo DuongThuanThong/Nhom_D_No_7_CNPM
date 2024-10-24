12845a590b3036a44a360fc4398c3877463a84c3
Thocuteee <thoch3765@ut.edu.vn>
2024-10-24 10:17:09 +0700

    tho sign up

--- WebApplication1/WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/WebApplication1/Pages/Index.cshtml.cs
@@ -1,20 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace WebApplication1.Pages
 {
-    public class IndexModel : PageModel
+    public class Index : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ILogger<Index> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public Index(ILogger<Index> logger)
         {
             _logger = logger;
         }
 
         public void OnGet()
         {
-
         }
     }
 }
--- WebApplication1/WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d409b6854e360482fb62a932c385a1c4e59713e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9f148192f39049662a28ec29516b77e99e7d436")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
