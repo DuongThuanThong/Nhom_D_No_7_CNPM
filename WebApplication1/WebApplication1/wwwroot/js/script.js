91a18fcdd9fb2d51aa12dafb2db6cafd4cc8cf39
dat <datnt1894@ut.edu.vn>
2024-10-25 10:17:50 +0700

    dat

---
+++ WebApplication1/WebApplication1/wwwroot/js/script.js
@@ -0,0 +1,13 @@
+﻿const togglePassword = document.getElementById('togglePassword');
+const passwordInput = document.getElementById('password');
+const eyeIcon = document.getElementById('eyeIcon');
+
+togglePassword.addEventListener('click', function () {
+    // Toggle giữa type 'password' và 'text'
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+
+    // Tùy chọn: Thay đổi icon
+    eyeIcon.classList.toggle('fa-eye'); // Hiện
+    eyeIcon.classList.toggle('fa-eye-slash'); // Ẩn
+});
