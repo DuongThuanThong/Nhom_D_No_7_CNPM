3805b9c377366c655b5546a6330b6b5a1661f007
Thocuteee <thoch3765@ut.edu.vn>
2024-10-27 16:59:34 +0700

    Tho-sign-up

--- WebApplication1/WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12845a590b3036a44a360fc4398c3877463a84c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8059349ff028628743b08f8deab5f6548549c5b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
